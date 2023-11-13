4f0bbdbe123d5c512a60b58179617943b0d913ec
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-11-13 16:19:24 -0300

    Feat: req 04

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,34 @@
+import { EnergyType } from "../Energy";
+
+abstract class Archetype {
+  _name: string;
+  _special: number;
+  _cost: number;
+  zeroNumber = 0;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = this.zeroNumber;
+    this._cost = this.zeroNumber;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+  
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
